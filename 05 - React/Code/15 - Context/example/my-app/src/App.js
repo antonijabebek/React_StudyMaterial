acecdd670dc1c2e006c23378ec96fc2b4bc6dc69
antonijabebek <137201277+antonijabebek@users.noreply.github.com>
2023-12-04 21:02:03 +0100

    update

--- 05 - React/Code/15 - Context/example/my-app/src/App.js
+++ 05 - React/Code/15 - Context/example/my-app/src/App.js
@@ -1,20 +1,20 @@
 import './App.css';
 import React from 'react';
 
-const userContext = React.createContext() 
+const userContext = React.createContext()
 
 export default function App() {
   return (
     <div>
-      <userContext.Provider value= {{
+      <userContext.Provider value={{
         user: 'Nino Škuflić',
         lastAccess: '4/12/2023 20:24:20 CEST',
         didLogOut: true
       }}>
-{/*Pozivati komponente*/}
+        {/*Pozivati komponente*/}
       </userContext.Provider>
-    
     </div>
+  
   );
 }
 
@@ -28,3 +28,25 @@ function User() {
     </userContext.Consumer>
   )
 }
+
+function LoggedOutUser() {
+  return (
+    <userContext.Consumer>
+      {value => <h1> Logged out User Data</h1>}
+      <p> The logged out user is: {value.user} </p>
+      <p> User logged out: {value.didLogOut ? "yes, user is logged out." : "no, user is not logged out."}</p>
+    </userContext.Consumer>
+  )
+}
+
+function LoggedInUser() {
+  return (
+    <userContext.Consumer>
+      {value => <h1> Logged in User Data</h1>}
+      <p> The logged in user is: {value.user} </p>
+      <p> User logged in: {value.didLogOut ? "yes, user is logged in." : "no, user is not logged in."}</p>
+    </userContext.Consumer>
+  )
+}
+
+
